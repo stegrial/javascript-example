9f6ec8708b135a6f371d378e8023eb20fa971c91
Grigoriy Stetsenko <stetsenko@softesis.com>
2019-08-01 16:11:42 +0300

    adding implicit wait

--- test/specs/exampleTest.js
+++ test/specs/exampleTest.js
@@ -4,10 +4,11 @@ const { By, until, Builder } = require('selenium-webdriver');
     const driver = new Builder().forBrowser('chrome').build();
 
     try {
+        await driver.manage().setTimeouts( { implicit: 5000 } );
         await driver.get('https://trueautomation.io');
 
         await driver.findElement(By.css('header div.jet-button__container > a')).click();
-        await driver.wait(until.titleIs('TrueAutomation.IO'), 1000);
+        // await driver.wait(until.titleIs('TrueAutomation.IO'), 1000);
 
         await driver.findElement(By.css('div.sign-up-container > a')).click();
         await driver.findElement(By.name('email')).sendKeys('your@gmail.com');
